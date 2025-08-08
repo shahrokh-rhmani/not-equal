a30b77611c6c2a2fdb8e79fd1af3e79293c6dbc5
shah <shahrokh.r1379@gmail.com>
2025-08-08 11:05:00 +0330

    Add user login functionality with custom template

--- src/config/urls.py
+++ src/config/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('shop.urls')),
-
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
