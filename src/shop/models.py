d7ad49383e61ce339a91f88d397a1f72203b6232
shah <shahrokh.r1379@gmail.com>
2025-09-24 10:09:29 +0330

    Add initial product fixtures

--- src/shop/models.py
+++ src/shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
