a5135472913fdf6b1ed55a87da3d864c6f335c0b
shah <shahrokh.r1379@gmail.com>
2025-09-24 10:14:08 +0330

    Add Product model, fixtures, list view and tests

--- src/config/settings.py
+++ src/config/settings.py
@@ -126,3 +126,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
--- src/shop/views.py
+++ src/shop/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Product
+from django.db.models import Q
 
-# Create your views here.
+def list_view(request):
+
+    method1 = Product.objects.exclude(price=1000)
+    method2 = Product.objects.filter(~Q(price=1000))
+    method3 = Product.objects.exclude(price__lt=1000).exclude(price__gt=1000)
+    combined = Product.objects.filter(is_available=True).exclude(price=1000)
+    
+    context = {
+        'method1': method1,
+        'method2': method2,
+        'method3': method3,
+        'combined': combined,
+    }
+    
+    return render(request, 'list_view.html', context)
